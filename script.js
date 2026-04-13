88352dec042b6ad64e3d0733905105a3b6002e60
novavaltara10 <ishikaambhoreambhore@gmail.com>
2026-04-13 20:31:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function addToCart(product, price) {
   alert(product + " added to cart 🛒");
   localStorage.setItem("cart", JSON.stringify(cart));
 }
+function searchProduct() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    let title = card.querySelector("h3").innerText.toLowerCase();
+    if (title.includes(input)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
