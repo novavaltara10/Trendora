25b44b66fad99f145cf36f122eca5bdfb9a10323
novavaltara10 <ishikaambhoreambhore@gmail.com>
2026-04-13 20:55:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ function addToCart(product, price) {
   alert(product + " added to cart 🛒");
   localStorage.setItem("cart", JSON.stringify(cart));
 }
+
 function searchProduct() {
   let input = document.getElementById("search").value.toLowerCase();
   let cards = document.querySelectorAll(".card");
 
   cards.forEach(card => {
     let title = card.querySelector("h3").innerText.toLowerCase();
+
     if (title.includes(input)) {
       card.style.display = "block";
     } else {
